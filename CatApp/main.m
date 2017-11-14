f45b415f59d5f5ca47e1a03bd4018929b4db8826
Sunny <sunnylee945@hotmail.com>
2017-11-14 09:42:57 +0800

    Initial Commit

---
+++ CatApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CatApp
+//
+//  Created by Mac on 2017/11/14.
+//  Copyright © 2017年 Sunny Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
